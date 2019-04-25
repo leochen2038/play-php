1854aabdeceb64d6e0b11eb3726bdda2b1ba3921
leo <leochen2038@gmail.com>
2019-04-25 10:20:26 +0800

    修改版本号

--- php_play.h
+++ php_play.h
@@ -24,7 +24,7 @@
 extern zend_module_entry play_module_entry;
 #define phpext_play_ptr &play_module_entry
 
-#define PHP_PLAY_VERSION "2.1.1" /* Replace with version number for your extension */
+#define PHP_PLAY_VERSION "2.1.3" /* Replace with version number for your extension */
 
 #ifdef PHP_WIN32
 #	define PHP_PLAY_API __declspec(dllexport)
